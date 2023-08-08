e8153a24c4d570aabf85fa0ebfe950bd854c3ec2
Ruel Israel <elurl115@gmail.com>
2023-08-08 21:25:03 +0800

    Update cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://ccerms-nuxt.vercel.app'],
 
     'allowed_origins_patterns' => [],
 

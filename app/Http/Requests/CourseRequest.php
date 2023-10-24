5df9447d767405f0b03ea970a4cba719aad64e88
IJ Subang <subangjay@gmail.com>
2023-10-24 09:41:55 +0800

    Updated course_code key

--- app/Http/Requests/CourseRequest.php
+++ app/Http/Requests/CourseRequest.php
@@ -37,7 +37,7 @@ class CourseRequest extends FormRequest
     public function postValidation(): array
     {
         return [
-            'key' => ['required', 'string', 'max:50', Rule::unique('courses')],
+            'course_code' => ['required', 'string', 'max:50', Rule::unique('courses')],
             'label' => ['required', 'string', 'max:50', Rule::unique('courses')],
             'milestone_one' => ['required'],
             'milestone_two' => ['required']
@@ -47,7 +47,7 @@ class CourseRequest extends FormRequest
     public function putValidation(): array
     {
         return [
-            'key' => ['required', 'string', 'max:50', Rule::unique('courses')->ignore($this->course->id)],
+            'course_code' => ['required', 'string', 'max:50', Rule::unique('courses')->ignore($this->course->id)],
             'label' => ['required', 'string', 'max:50', Rule::unique('courses')->ignore($this->course->id)],
             'milestone_one' => ['required'],
             'milestone_two' => ['required']
